1622712cefca6e29e0339655ce2416fa934ed8e9
Yuri Makarenko <yuri.makarenko@umbrella-web.com>
2016-05-27 16:25:49 +0300

    test

--- web/public-src/js/app.js
+++ web/public-src/js/app.js
@@ -1,20 +1,20 @@
 'use strict';
 
-$(function(){
-	/**
-	 * Add resize handler for resize iframe in jira
-	 */
-	var appWidth = false;
-	$(window).resize(function(){
-		var $app = $('#checklists');
-		if ( ! appWidth ) {
-			appWidth = $app.width();
-		}
-		console.log('Set width: ' + appWidth);
-		AP.resize(appWidth, $app.height());
-	});
-
-});
+//$(function(){
+//	/**
+//	 * Add resize handler for resize iframe in jira
+//	 */
+//	var appWidth = false;
+//	$(window).resize(function(){
+//		var $app = $('#checklists');
+//		if ( ! appWidth ) {
+//			appWidth = $app.width();
+//		}
+//		console.log('Set width: ' + appWidth);
+//		AP.resize(appWidth, $app.height());
+//	});
+//
+//});
 
 
 var angularApplication = angular.module('checklistsApp', ['ui.sortable', 'ngSanitize', 'colorpicker.module']).config(function($interpolateProvider){
