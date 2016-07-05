1cd6aba38269b63adf8d3e8e00dc3a81c12c96ce
Yuri Makarenko <yuri.makarenko@umbrella-web.com>
2016-07-05 23:30:34 +0300

    Add redis bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new AtlassianConnectBundle\AtlassianConnectBundle(),
+            new Snc\RedisBundle\SncRedisBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
