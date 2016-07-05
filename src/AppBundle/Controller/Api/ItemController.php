a9d131dd9804d47b2f04723c2a97d507a19d4394
Yuri Makarenko <yuri.makarenko@umbrella-web.com>
2016-07-05 20:50:00 +0300

    Add comments to controllers

--- src/AppBundle/Controller/Api/ItemController.php
+++ src/AppBundle/Controller/Api/ItemController.php
@@ -19,24 +19,32 @@ class ItemController extends Controller
      */
     public function createItemAction(Request $request, $projectKey, $issueKey, $issueId) 
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
         
+        // retrieve POST parameters
         $checklistId = $request->request->get('checklist_id');
         $itemText = $request->request->get('item_text');
         $color = $request->request->get('color');
         
+        // check require parameters 
         if ( ! $checklistId OR ! $itemText ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for item
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
+            // return fail if issue not found
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
         
+        // create new item
         $item = $this->get('item')->createNewItem($issue, $checklistId, $itemText, $color);
+        
+        // return create item result
         return new JsonResponse( array('status' => (bool)$item, 'item_id' => $item ? $item->getId() : 0 ) );
     }
 
@@ -50,25 +58,32 @@ class ItemController extends Controller
      */
     public function updateItemAction(Request $request, $projectKey, $issueKey, $issueId)
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
 
+        // retrieve POST parameters
         $checklistId = $request->request->get('checklist_id');
         $itemId = $request->request->get('item_id');
         $newItemText = $request->request->get('item_text');
         $color = $request->request->get('color');
 
+        // check require parameters 
         if ( ! $checklistId OR ! $itemId OR ! $newItemText ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for item
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
 
+        // update existing item
         $result = $this->get('item')->updateItem($issue, $checklistId, $itemId, $newItemText, $color);
+        
+        // return update item result
         return new JsonResponse( array('status' => $result) );
     }
     
@@ -82,23 +97,30 @@ class ItemController extends Controller
      */
     public function removeItemAction(Request $request, $projectKey, $issueKey, $issueId)
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
 
+        // retrieve POST parameters
         $checklistId = $request->request->get('checklist_id');
         $itemId = $request->request->get('item_id');
-        
+
+        // check require parameters 
         if ( ! $checklistId OR ! $itemId ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for item
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
         
+        // remove existing item
         $result = $this->get('item')->removeItem($issue, $checklistId, $itemId);
+
+        // return remove item result
         return new JsonResponse( array('status' => $result ) );
     }
 
@@ -112,28 +134,35 @@ class ItemController extends Controller
      */
     public function completeItemAction(Request $request, $projectKey, $issueKey, $issueId) 
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
 
+        // retrieve POST parameters
         $checklistId = $request->request->get('checklist_id');
         $itemId = $request->request->get('item_id');
 
+        // check require parameters 
         if ( ! $checklistId OR ! $itemId ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for item
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
 
+        // complete item
         $item = $this->get('item')->completeItem($issue, $checklistId, $itemId);
         
+        // return false status on fail action
         if ( ! $item ) {
             return new JsonResponse( array('status' => false ) );
         }
-        
+
+        // return complete item result
         return new JsonResponse( array('status' => true, 'checked' => $item->getChecked() ) );
     }
 
@@ -147,22 +176,29 @@ class ItemController extends Controller
      */
     public function sortableUpdateAction(Request $request, $projectKey, $issueKey, $issueId)
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
 
+        // retrieve POST parameters
         $orders = $request->request->get('checklists_sort');
 
+        // check require parameters 
         if ( ! $orders OR ! is_array( $orders ) ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for item
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
 
+        // sort items
         $result = $this->get('item')->sortItems($issue, $orders);
+        
+        // return sort item result
         return new JsonResponse( array('status' => $result ) );
     }
 }
--- src/AppBundle/Controller/api/ChecklistController.php
+++ src/AppBundle/Controller/api/ChecklistController.php
@@ -19,22 +19,29 @@ class ChecklistController extends Controller
      */
     public function createChecklistAction(Request $request, $projectKey, $issueKey, $issueId) 
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
-        
+
+        // retrieve POST parameters
         $checklistName = $request->request->get('name');
-        
+
+        // check require parameters 
         if ( ! $checklistName ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for checklist
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
         
+        // create new checklist
         $checklist = $this->get('checklist')->createNewChecklist($issue, $checklistName);
+        
+        // return create new checklist result
         return new JsonResponse( array('status' => true, 'checklist_id' => $checklist->getId() ) );
     }
 
@@ -48,23 +55,30 @@ class ChecklistController extends Controller
      */
     public function updateChecklistAction(Request $request, $projectKey, $issueKey, $issueId)
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
 
+        // retrieve POST parameters
         $checklistId = $request->request->get('id');
         $newChecklistName = $request->request->get('name');
 
+        // check require parameters 
         if ( ! $checklistId OR ! $newChecklistName ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for checklist
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
 
+        // update existing checklist
         $result = $this->get('checklist')->updateChecklist($issue, $checklistId, $newChecklistName);
+        
+        // return update existing checklist result
         return new JsonResponse( array('status' => $result) );
     }
     
@@ -78,22 +92,29 @@ class ChecklistController extends Controller
      */
     public function removeChecklistAction(Request $request, $projectKey, $issueKey, $issueId)
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
 
+        // retrieve POST parameters
         $checklistId = $request->request->get('id');
 
+        // check require parameters 
         if ( ! $checklistId ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for checklist
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
         
+        // remove existing checklist
         $result = $this->get('checklist')->removeChecklist($issue, $checklistId);
+        
+        // return remove existing checklist result
         return new JsonResponse( array('status' => $result ) );
     }
 
@@ -107,22 +128,29 @@ class ChecklistController extends Controller
      */
     public function sortableUpdateAction(Request $request, $projectKey, $issueKey, $issueId)
     {
+        // available only POST requests for API
         if ( $request->getMethod() != 'POST') {
             return new JsonResponse(array('status' => false, 'message' => 'available only POST request'));
         }
-        
+
+        // retrieve POST parameters
         $orders = $request->request->get('orders');
 
+        // check require parameters 
         if ( ! $orders OR ! is_array( $orders ) ) {
             return new JsonResponse(array('status' => false, 'message' => 'needed data is lost'));
         }
 
+        // find issue for checklist
         $issue = $this->getDoctrine()->getRepository('AppBundle:Issue')->findOneIssue($projectKey, $issueId, $issueKey);
         if ( ! $issue ) {
             return new JsonResponse(array('status' => false, 'message' => 'Issue is not found'));
         }
 
+        // sort checklists
         $result = $this->get('checklist')->sortChecklists($issue, $orders);
+        
+        // return sort checklists result
         return new JsonResponse( array('status' => $result ) );
     }
 }
